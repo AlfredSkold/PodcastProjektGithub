86a4a42929cd0e2f980f7465816846e21bd318fd
AlfredSkold <Alfredskold7@gmail.com>
2017-10-17 14:39:39 +0200

    Buttonnn

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -174,7 +174,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(223, 98);
+            this.button5.Location = new System.Drawing.Point(226, 236);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 15;
